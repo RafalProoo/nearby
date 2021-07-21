c1ed6fdeefdac5ae7f7b8f13fbae9a043461ccae
Barabasz.Rafal <239503@edu.p.lodz.pl>
2021-07-21 23:17:56 +0200

    Created simple account view

--- lib/account_view.dart
+++ lib/account_view.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_neumorphic_null_safety/flutter_neumorphic.dart';
 
 class AccountView extends StatelessWidget {
@@ -5,6 +7,25 @@ class AccountView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text("Account");
+    return ListView(
+      children: [
+        const Icon(Icons.account_circle, size: 128, color: Color(0xff1b58ca)),
+        Center(
+            child: Text(
+          FirebaseAuth.instance.currentUser!.email.toString(),
+          style: const TextStyle(fontSize: 20, color: Colors.black87),
+        )),
+        TextButton(
+          onPressed: () async {
+            await FirebaseAuth.instance.signOut();
+            Navigator.popAndPushNamed(context, '/signIn');
+          },
+          child: const Text(
+            'Sign out',
+            style: TextStyle(color: Color(0xff1b58ca)),
+          ),
+        )
+      ],
+    );
   }
 }
